cdc604a80b88e4341ded98b975401f3179ed2dbb
azishio <azishio3141@gmail.com>
2024-05-21 18:38:37 +0900

    📈 ベンチマークの追加

---
+++ benches/response1.rs
@@ -0,0 +1,28 @@
+use criterion::{Criterion, criterion_group, criterion_main};
+use csv::Reader;
+
+use seismic_response::{ResponseAccAnalyzer, ResponseAccAnalyzerParams};
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let mut csv = Reader::from_path("benches/seismic_acc_waveform.csv").unwrap();
+    let data = csv.deserialize::<f64>().map(|x| x.unwrap()).collect::<Vec<_>>();
+
+    let params = ResponseAccAnalyzerParams {
+        natural_period_ms: 500,
+        dt_ms: 10,
+        mass: 100.,
+        damping_h: 0.05,
+        beta: 0.25,
+        init_x: 0.0,
+        init_v: 0.0,
+        init_a: 0.0,
+        init_xg: 0.0,
+    };
+
+    let analyzer = ResponseAccAnalyzer::from_params(params);
+
+    c.bench_function("response1", |b| b.iter(|| analyzer.analyze(data.clone())));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
